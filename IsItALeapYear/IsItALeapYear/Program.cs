fbea4efc9613c948fb93e9a2c539dbc6dc39a77f
Neel Patel <npatel16@confederationcollege.ca>
2021-06-09 14:16:17 -0400

    Folder is added

---
+++ IsItALeapYear/IsItALeapYear/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool IsItALeapYear(int Year)
+        {
+            if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) return true;
+            else return false;
+        }
+    }
+}
